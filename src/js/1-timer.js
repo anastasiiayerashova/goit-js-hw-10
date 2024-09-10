368ceda03e521ea44179ff4b7bf8a97b46ecae7d
Anastasiia Yerashova <yerashova.a@gmail.com>
2024-09-10 23:07:37 +0200

    done 1

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -52,15 +52,15 @@ startBtn.addEventListener("click", () => {
         const timeDifference = userSelectedDate - currentTime;
         if (timeDifference <= 0) {
             clearInterval(intervalId);
-            updateTimer(0);
+            addLeadingZero(0);
             datetimePicker.disabled = false;
             return;
         }
-        updateTimer(timeDifference);
+        addLeadingZero(timeDifference);
     }, 1000);
 });
 
-function updateTimer(ms) {
+function addLeadingZero(ms) {
     const { days, hours, minutes, seconds } = convertMs(ms);
     document.querySelector("[data-days]").textContent = days;
     document.querySelector("[data-hours]").textContent = String(hours).padStart(2, '0');
@@ -69,27 +69,22 @@ document.querySelector("[data-seconds]").textContent = String(seconds).padStart(
 }
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+console.log(convertMs(2000)); 
+console.log(convertMs(140000)); 
+console.log(convertMs(24140000)); 
 
 
 
