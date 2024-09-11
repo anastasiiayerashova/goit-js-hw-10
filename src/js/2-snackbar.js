ed582b2de12495a16a367195c1bf82446e08713d
Anastasiia Yerashova <yerashova.a@gmail.com>
2024-09-11 22:25:36 +0200

    done

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,21 +1,21 @@
-
-
 import iziToast from "izitoast";
-
 import "izitoast/dist/css/iziToast.min.css";
+
 document.head.insertAdjacentHTML("beforeend", `<link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital@0;1&display=swap" rel="stylesheet"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 `);
 
 const form = document.querySelector(".form");
-
+const labelElement = document.querySelector('label');
+labelElement.classList.add("css-delay");
+    
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-  const delayInput = document.querySelector("input[name='delay']");
-const inputF = document.querySelector("input[value='fulfilled']").checked;
+    const delayInput = document.querySelector("input[name='delay']");
+    const inputF = document.querySelector("input[value='fulfilled']").checked;
     const inputR = document.querySelector("input[value='rejected']").checked;
-    
+  
     let delay = Number(delayInput.value);
     
     const fetchData = ({ value, delay, isSuccess }) => {
@@ -49,6 +49,7 @@ const inputF = document.querySelector("input[value='fulfilled']").checked;
               class: "custom-toast",
               message: value, }));
     }
+    
     else if (inputR) {
         fetchData({
             value: `Rejected promise in ${delay}ms`,
